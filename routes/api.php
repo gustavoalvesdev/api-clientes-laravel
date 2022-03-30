8e2eb35b5438ae728cea7bdc031a2f9fc2e25d3a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:07:39 -0300

    definicao das rotas basicas na api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('customers/', [CustomerController::class, 'all']);
+Route::get('customer/{id^}', [CustomerController::class, 'one']);
+Route::post('customer/', [CustomerController::class, 'add']);
+Route::put('customer/{id}', [CustomerController::class, 'change']);
+Route::delete('customer/{id}', [CustomerController::class, 'delete']);
