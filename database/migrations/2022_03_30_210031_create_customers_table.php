98934e80c175ab816dbb1769356c7cef5f953ea5
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:03:39 -0300

    criacao da migration de clientes

---
+++ database/migrations/2022_03_30_210031_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf_cnpj_customer')->unique();
+            $table->string('email_customer')->nullable();
+            $table->string('address_customer');
+            $table->date('birth_customer')->nullable();
+            $table->string('name_customer');
+            $table->string('phone_customer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
