b91fed8b3e7f91cb2295e9ebc7e92c9f0a3bbea9
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:10:15 -0300

    definicao dos metodos no controller de clientes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function all()
+    {
+        // TODO: all method implementation
+    }
+
+    public function one($id)
+    {
+        // TODO: one method implementation
+    }
+
+    public function add(Request $request)
+    {
+        // TODO: add method implementation
+    }
+
+    public function change(Request $request, $id)
+    {
+        // TODO: change method implementation
+    }
+
+    public function delete($id)
+    {
+        // TODO: delete method implementation
+    }
 }
