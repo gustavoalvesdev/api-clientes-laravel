b1f847a9acc0655e55585f4022fd378b71446078
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:13:07 -0300

    remocao do import desnecessario da classe Request no api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('customers/', [CustomerController::class, 'all']);
