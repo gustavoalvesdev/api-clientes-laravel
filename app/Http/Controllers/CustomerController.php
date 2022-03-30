4aa6e10d040c77c6ea352723f8c93ed349d7cdd1
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:33:51 -0300

    insercao de clientes com validacao  funcionando

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -40,7 +41,59 @@ class CustomerController extends Controller
 
     public function add(Request $request)
     {
-        // TODO: add method implementation
+        $array = ['error' => ''];
+
+        $rules = [
+            'cpf_cnpj' => 'required|min:11',
+            'email_customer' => 'email',
+            'address_customer' => 'required|min:10',
+            'birth_customer' => 'date',
+            'name' => 'required|min:5',
+            'phone' => 'min:8'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+
+            $array['error'] = $validator->getMessageBag();
+
+        } else {
+
+            $cpf_cnpj = $request->input('cpf_cnpj');
+            $email_customer = $request->input('email_customer');
+            $address_customer = $request->input('address_customer');
+            $birth_customer = $request->input('birth_customer');
+            $name = $request->input('name');
+            $phone = $request->input('phone');
+
+            $customer = new Customer();
+
+            $customer->cpf_cnpj_customer = $cpf_cnpj;
+
+            if ($email_customer) {
+                $customer->email_customer = $email_customer;
+            }
+
+            $customer->address_customer = $address_customer;
+
+            if ($birth_customer) {
+                $customer->birth_customer = $birth_customer;
+            }
+
+            $customer->name_customer = $name;
+
+            if ($phone) {
+                $customer->phone_customer = $phone;
+            }
+
+            $customer->save();
+
+            $array['success'] = 'Cliente cadastrado com sucesso!';
+
+        }
+
+        return $array;
     }
 
     public function change(Request $request, $id)
