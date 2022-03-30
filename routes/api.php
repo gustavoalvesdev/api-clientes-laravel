2eac3851494b853ccb8ac6011a19d3c2f3eb3fc3
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-30 18:15:56 -0300

    correcao de erro de digitacao na definicao da rota de busca de um cliente

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('customers/', [CustomerController::class, 'all']);
-Route::get('customer/{id^}', [CustomerController::class, 'one']);
+Route::get('customer/{id}', [CustomerController::class, 'one']);
 Route::post('customer/', [CustomerController::class, 'add']);
 Route::put('customer/{id}', [CustomerController::class, 'change']);
 Route::delete('customer/{id}', [CustomerController::class, 'delete']);
